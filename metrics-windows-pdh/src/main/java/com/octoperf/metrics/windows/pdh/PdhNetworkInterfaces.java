f0fadfbb02d6814f7824fab9ada093774447f986
jloisel <loisel.jerome@gmail.com>
2016-09-29 10:33:41 +0200

    logs

--- metrics-windows-pdh/src/main/java/com/octoperf/metrics/windows/pdh/PdhNetworkInterfaces.java
+++ metrics-windows-pdh/src/main/java/com/octoperf/metrics/windows/pdh/PdhNetworkInterfaces.java
@@ -1,10 +1,11 @@
 package com.octoperf.metrics.windows.pdh;
 
-import com.octoperf.metrics.windows.pdh.api.WindowsNetworkInterfaceMetrics;
-import com.octoperf.metrics.windows.pdh.api.PerfmonQueryService;
 import com.octoperf.metrics.service.api.GaugeService;
+import com.octoperf.metrics.windows.pdh.api.PerfmonQueryService;
+import com.octoperf.metrics.windows.pdh.api.WindowsNetworkInterfaceMetrics;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.hyperic.sigar.win32.Pdh;
 import org.hyperic.sigar.win32.Win32Exception;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -13,10 +14,12 @@ import org.springframework.stereotype.Component;
 
 import javax.management.JMException;
 import javax.management.ObjectName;
+import java.util.List;
 
 import static java.lang.String.format;
 import static javax.management.ObjectName.quote;
 
+@Slf4j
 @Component
 @ConditionalOnBean(Pdh.class)
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -30,7 +33,9 @@ final class PdhNetworkInterfaces {
     final AnnotationMBeanExporter exporter) throws Win32Exception, JMException {
     super();
 
-    for(final String instance: perfmon.getInstances(IFACE)) {
+    final List<String> instances = perfmon.getInstances(IFACE);
+    log.info("Network Interfaces: " + instances);
+    for(final String instance: instances) {
       final ObjectName objectName = new ObjectName(format(OBJECT_NAME, quote(instance)));
       final WindowsNetworkInterfaceMetrics metric = new PdhNetworkInterfaceMetrics(perfmon, gauges, instance);
       exporter.registerManagedResource(metric, objectName);
--- metrics-windows-pdh/src/main/java/com/octoperf/metrics/windows/pdh/PdhPhysicalDisks.java
+++ metrics-windows-pdh/src/main/java/com/octoperf/metrics/windows/pdh/PdhPhysicalDisks.java
@@ -1,11 +1,11 @@
 package com.octoperf.metrics.windows.pdh;
 
-import com.octoperf.metrics.windows.pdh.api.WindowsPhysicalDiskMetrics;
-import com.octoperf.metrics.windows.pdh.api.PerfmonQueryService;
 import com.octoperf.metrics.service.api.GaugeService;
-import com.octoperf.metrics.windows.pdh.api.WindowsSystemMetrics;
+import com.octoperf.metrics.windows.pdh.api.PerfmonQueryService;
+import com.octoperf.metrics.windows.pdh.api.WindowsPhysicalDiskMetrics;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.hyperic.sigar.win32.Pdh;
 import org.hyperic.sigar.win32.Win32Exception;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -15,9 +15,12 @@ import org.springframework.stereotype.Component;
 import javax.management.JMException;
 import javax.management.ObjectName;
 
+import java.util.List;
+
 import static java.lang.String.format;
 import static javax.management.ObjectName.quote;
 
+@Slf4j
 @Component
 @ConditionalOnBean(Pdh.class)
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -31,7 +34,9 @@ final class PdhPhysicalDisks {
       final AnnotationMBeanExporter exporter) throws Win32Exception, JMException {
     super();
 
-    for(final String instance: perfmon.getInstances(PHYSICAL_DISK)) {
+    final List<String> instances = perfmon.getInstances(PHYSICAL_DISK);
+    log.info("Physical Disks: " + instances);
+    for(final String instance: instances) {
       final ObjectName objectName = new ObjectName(format(OBJECT_NAME, PHYSICAL_DISK, quote(instance)));
       final WindowsPhysicalDiskMetrics metric = new PdhPhysicalDiskMetrics(perfmon, gauges, instance);
       exporter.registerManagedResource(metric, objectName);
